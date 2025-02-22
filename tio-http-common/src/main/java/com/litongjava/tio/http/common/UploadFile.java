2761863d231d93a98ede3bd1ac6773d50d38d46f
litongjava <litongjava@qq.com>
2025-02-21 21:50:06 -1000

    change to 3.7.3.v20250301-RELEASE

--- tio-http-common/src/main/java/com/litongjava/tio/http/common/UploadFile.java
+++ tio-http-common/src/main/java/com/litongjava/tio/http/common/UploadFile.java
@@ -1,56 +1,19 @@
 package com.litongjava.tio.http.common;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 /**
- *
- * @author tanyaowu
- * 2017年7月26日 下午3:12:56
+ * UploadFile
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain=true)
 public class UploadFile {
-
-	private String	name	= null;
-	private int		size	= -1;
-
-	private byte[] data = null;
-	//    private File file = null;
-
-	/**
-	 *
-	 */
-	public UploadFile() {
-
-	}
-
-	public byte[] getData() {
-		return data;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public int getSize() {
-		return size;
-	}
-
-	public void setData(byte[] data) {
-		this.data = data;
-	}
-
-	//    public File getFile()
-	//    {
-	//        return file;
-	//    }
-	//
-	//    public void setFile(File file)
-	//    {
-	//        this.file = file;
-	//    }
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setSize(int size) {
-		this.size = size;
-	}
+  private String name = null;
+  private long size = -1;
+  private byte[] data = null;
 }
