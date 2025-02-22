dc918c38bc75446736ae57caadbd9146ec10715d
litongjava <litongjava@qq.com>
2025-02-21 22:13:37 -1000

    update UploadFile

--- tio-http-common/src/main/java/com/litongjava/tio/http/common/UploadFile.java
+++ tio-http-common/src/main/java/com/litongjava/tio/http/common/UploadFile.java
@@ -11,9 +11,15 @@ import lombok.experimental.Accessors;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Accessors(chain=true)
+@Accessors(chain = true)
 public class UploadFile {
+
   private String name = null;
   private long size = -1;
   private byte[] data = null;
+
+  public UploadFile(String filename, byte[] bytes) {
+    this.name = filename;
+    this.data = bytes;
+  }
 }
